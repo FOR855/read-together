2379a1e3d081fe26fc70219b722810aadc7ea63d
FOR855 <111327750+FOR855@users.noreply.github.com>
2025-08-08 10:44:47 +0800

    implemented fake user profile entry

--- src/app/components/UserProfile.js
+++ src/app/components/UserProfile.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Link from "next/link";
+import styles from "./UserProfile.module.css";
 
 function UserProfile() {
-  return <div>UserProfile</div>;
+  return (
+    <div className={styles.box}>
+      <Link href="/user" className={styles.link}>
+        <img
+          src="/icons/yonghu.svg"
+          alt="about read-together"
+          className={styles.linkIcon}
+        />
+      </Link>
+    </div>
+  );
 }
 
 export default UserProfile;
