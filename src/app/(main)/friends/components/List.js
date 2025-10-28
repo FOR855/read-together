ab2ac54718841efbbcb9144ed840ae35e9d69f76
FOR855 <111327750+FOR855@users.noreply.github.com>
2025-10-28 16:11:29 +0800

    friendsList1

--- src/app/(main)/friends/components/List.js
+++ src/app/(main)/friends/components/List.js
@@ -1,8 +1,38 @@
 import React from "react";
 import styles from "../page.module.css";
+import AFriend from "./AFriend";
 
-function List() {
-  return <div>List</div>;
+async function List() {
+  const session = await auth();
+  if (!session || !session.user.email) {
+    // Not signed in — redirect or show login link
+    return <p>You must sign in to view the list.</p>;
+  }
+
+  // session.user.email should be populated if you forwarded it via callbacks
+  const email = session.user.email;
+  console.log("Fetching freinds list for email:", email);
+  const userList = await sql`SELECT * FROM users WHERE email=${email}`;
+  const user = userList[0];
+
+  const friendsList = await sql`SELECT users.*
+FROM friends
+JOIN users
+  ON users.id = CASE 
+                  WHEN friends.user_id_1 = ${user.id} THEN friends.user_id_2
+                  ELSE friends.user_id_1
+                END
+WHERE ${user.id} IN (friends.user_id_1, friends.user_id_2)
+  AND friends.status = 'accepted';`;
+  // const onlineFriends = friendsList.filter((f) => f.online);
+  // const offlineFriends = friendsList.filter((f) => !f.online);
+
+  return (
+    <div className={styles.friendsList}>
+      <div className={styles.header}>My Friends</div>
+      <AFriend />
+    </div>
+  );
 }
 
 export default List;
